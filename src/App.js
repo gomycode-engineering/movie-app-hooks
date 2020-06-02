cbb39a96832628e4cdaf2138376cd3e03b8a0819
Anis Melliti <ansmeliti@gmail.com>
2020-06-02 17:54:25 +0100

    implement the solution

--- src/App.js
+++ src/App.js
@@ -1,8 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 import './App.css';
+import { moviesList } from './assets/mookData';
+import MoviesList from './components/MoviesList';
+import AddMovie from './components/AddMovie';
+import SearchBar from './components/SearchBar';
 
 function App() {
-  return <div className='App'></div>;
+  const [movieList, setMovieList] = useState(moviesList);
+  const [searchWord, setSearchWord] = useState('');
+  const [ratingSearch, setRatingSearch] = useState(0);
+
+  const addMovie = (newMovie) => setMovieList([...movieList, newMovie]);
+  const handleSearch = (e) => setSearchWord(e.target.value);
+
+  return (
+    <div className='container'>
+      <SearchBar
+        handleSearch={handleSearch}
+        setRatingSearch={setRatingSearch}
+        ratingSearch={ratingSearch}
+      />
+      <MoviesList
+        moviesArray={
+          searchWord
+            ? movieList.filter((movie) =>
+                movie.title.toLowerCase().includes(searchWord.toLowerCase())
+              )
+            : ratingSearch > 1
+            ? movieList.filter((movie) => movie.rate >= ratingSearch)
+            : movieList
+        }
+      />
+      <AddMovie handleAdd={addMovie} />
+    </div>
+  );
 }
 
 export default App;
